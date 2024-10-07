ded984eb4a7d8ec179f7a1a6ebe0ee21bfaf6a8e
Martti Grönholm <martti.gronholm@vincit.fi>
2024-10-07 10:28:43 +0300

    Add clerk middleware, sign-in and sign-up pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">AI Journal App</h1>
+        <p className="text-2xl text-white/60 mb-4">
+          AI-powered app for tracking your mood through the days.
+        </p>
+        <div>
+          <Link href="/journal">
+            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl hover:bg-blue-600/80">
+              Get started
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
