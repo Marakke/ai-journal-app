fddc7426cc9a61143dfa7cb67a6634dd7760dedb
Martti Grönholm <martti.gronholm@vincit.fi>
2024-10-10 14:26:55 +0300

    Add new OpenAI model to answer questions which remembers journal entries

--- utils/ai.ts
+++ utils/ai.ts
@@ -1,7 +1,10 @@
 import { PromptTemplate } from '@langchain/core/prompts'
-import { OpenAI } from '@langchain/openai'
+import { OpenAI, OpenAIEmbeddings } from '@langchain/openai'
 import { StructuredOutputParser } from 'langchain/output_parsers'
 import { z } from 'zod'
+import { Document } from 'langchain/document'
+import { loadQARefineChain } from 'langchain/chains'
+import { MemoryVectorStore } from 'langchain/vectorstores/memory'
 
 const parser = StructuredOutputParser.fromZodSchema(
   z.object({
@@ -55,4 +58,25 @@ export const analyze = async (content) => {
   } catch (e) {
     console.log(e)
   }
+}
+
+export const qa = async (question, entries) => {
+  const docs = entries.map(
+    (entry) =>
+      new Document({
+        pageContent: entry.content,
+        metadata: { source: entry.id, date: entry.createdAt },
+      })
+  )
+  const model = new OpenAI({ temperature: 0, modelName: 'gpt-3.5-turbo' })
+  const chain = loadQARefineChain(model)
+  const embeddings = new OpenAIEmbeddings()
+  const store = await MemoryVectorStore.fromDocuments(docs, embeddings)
+  const relevantDocs = await store.similaritySearch(question)
+  const res = await chain.call({
+    input_documents: relevantDocs,
+    question,
+  })
+
+  return res.output_text
 }
